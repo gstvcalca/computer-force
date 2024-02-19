b81e558a9a9222008be5b76f70de49eb6b143d4a
gstvc <gstv.calca@gmail.com>
2024-02-19 15:46:03 +1100

    feat: added use local storage hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T){
+    const [currValue, setValue] = useState(initialValue)
+
+    
+    useEffect(() => {
+            // undefined window means no access to localStorage
+            if (typeof window === 'undefined') return;
+            let value = localStorage.getItem(key)
+            if (value) setValue(JSON.parse(value))
+        }, [window]
+    )
+
+    const updateLocalStorage = (newValue: T) =>{
+        setValue(newValue);
+        localStorage.setItem(key, JSON.stringify(newValue));
+    }
+
+    return {
+        currValue,
+        updateLocalStorage
+    }
+}
